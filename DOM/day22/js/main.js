b67ad3ddc6de1222be83e3e2ab5830236f6c127d
Morning <morning@mstars-lab2-07s-iMac.local>
2022-12-14 12:21:00 +0800

    day22

---
+++ DOM/day22/js/main.js
@@ -0,0 +1,28 @@
+//add function
+
+
+function add(x,y) {
+    return x + y;
+}
+
+//print function
+
+function print(f) {
+    let x = 3 , y = 2;
+    console.log(f(x,y))
+}
+
+// callback function
+
+print(add);
+
+
+
+// setTimeout
+
+let x = () => {
+    console.log("arrow function");
+}
+
+
+setTimeout(x,2000);
